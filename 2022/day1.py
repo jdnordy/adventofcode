de43f50ebb04c729f6e293af36f5cb7dfd346e52
Joshua David Nordstrom <joshua@jdnordstrom.com>
2023-01-01 21:41:13 -0800

    complete part2 of day1

--- 2022/day1.py
+++ 2022/day1.py
@@ -2,15 +2,22 @@
 
 f = open('day1.inputs.txt', 'r')
 
-max = 0;
-current = 0;
+max = 0
+current = 0
+
+elfs = []
 
 for rawline in f.readlines():
     calories = rawline.strip()
     if not calories:
         if current > max: max = current
+
+        elfs.append(current)
+        
         current = 0
     else:
         current += int(calories)
 
 print(max)
+elfs.sort(reverse=True)
+print(elfs[0] + elfs[1] + elfs[2])
